4cb7d1056d1695d464e8849fd50a422ded521547
Márcio Santos <marcio.tbms@gmail.com>
2020-02-28 15:13:31 -0300

    Create component to manage state between editing meeting or not

---
+++ src/containers/Meetings/MeetingList/MeetingItem/MeetingEditItem/MeetingEditItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const MeetingEditItem: React.FC<Props> = () => {
+  return <div>EDITING</div>;
+};
+
+export default MeetingEditItem;
--- src/containers/Meetings/MeetingList/MeetingItem/MeetingItem.tsx
+++ src/containers/Meetings/MeetingList/MeetingItem/MeetingItem.tsx
@@ -1,51 +1,19 @@
-import React, { ReactElement } from 'react';
-import Card from '../../../../components/Card';
-import { Typography, IconButton, makeStyles } from '@material-ui/core';
-import { Content, Header } from './styles';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Chip from '../../../../components/Chip/Chip';
-import { Meeting, Task } from '../../../../../types';
-import { format } from 'date-fns';
+import React, { useState } from 'react';
+import { Meeting } from '../../../../../types';
+import MeetingDisplayItem from './MeetingDisplayItem';
+import MeetingEditItem from './MeetingEditItem';
 
 interface Props {
   meeting: Meeting;
+  editing?: boolean;
 }
 
-const useStyles = makeStyles(theme => ({
-  meetingDate: {
-    color: theme.palette.secondary.dark,
-  },
-}));
+const MeetingItem: React.FC<Props> = ({ meeting, editing = false }) => {
+  const [isEditing, setIsEditing] = useState<boolean>(editing);
 
-const MeetingItem: React.FC<Props> = ({ meeting }) => {
-  const classes = useStyles();
+  const handleToggleIsEditing = (): void => setIsEditing(!isEditing);
 
-  const { meetingDate, meetingTitle, checklist } = meeting;
-
-  const countDoneTasks = (acc: number, item: Task): number => (item.checked ? acc + 1 : acc);
-
-  const doneCount = checklist.reduce(countDoneTasks, 0);
-
-  return (
-    <Card onClick={(): void => console.log('BATATA')}>
-      <Content>
-        <div>
-          <Header>
-            <Typography variant="h6" color="secondary">
-              {meetingTitle}
-            </Typography>
-            <Chip color="primary" label={`${doneCount} de ${checklist.length}`} margin />
-          </Header>
-          <Typography variant="subtitle2" className={classes.meetingDate}>
-            {format(new Date(meetingDate), "dd 'de' MMM, yyyy")}
-          </Typography>
-        </div>
-        <IconButton color="secondary" aria-label="delete">
-          <DeleteIcon />
-        </IconButton>
-      </Content>
-    </Card>
-  );
+  return isEditing ? <MeetingEditItem /> : <MeetingDisplayItem meeting={meeting} onEdit={handleToggleIsEditing} />;
 };
 
 export default MeetingItem;

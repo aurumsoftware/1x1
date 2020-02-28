e9d2923b87cfbcc34c278f6d739bc78a162b3c91
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-28 15:36:56 -0300

    Creates constants

--- src/services/meetingService.ts
+++ src/services/meetingService.ts
@@ -1,12 +1,14 @@
 import api from './api';
 import { Meeting } from '../../types';
 
-const createMeeting = (meeting: Meeting): Promise<Response> => api.post('/meetings', meeting);
+const MEETINGS = '/meetings';
 
-const updateMeeting = (meeting: Meeting, id: string): Promise<Response> => api.put(`/meetings/${id}`, meeting);
+const createMeeting = (meeting: Meeting): Promise<Response> => api.post(MEETINGS, meeting);
 
-const listMeetings = (userId: string): Promise<Response> => api.get(`/meetings/${userId}`);
+const updateMeeting = (meeting: Meeting, id: string): Promise<Response> => api.put(`${MEETINGS}/${id}`, meeting);
 
-const removeMeeting = (id: string): Promise<Response> => api.delete(`/meetings/${id}`);
+const listMeetings = (userId: string): Promise<Response> => api.get(`${MEETINGS}/${userId}`);
+
+const removeMeeting = (id: string): Promise<Response> => api.delete(`${MEETINGS}/${id}`);
 
 export default { createMeeting, updateMeeting, listMeetings, removeMeeting };
--- src/services/privateNoteService.ts
+++ src/services/privateNoteService.ts
@@ -1,17 +1,19 @@
 import api from './api';
 import { PrivateNote } from '../../types';
 
-const createPrivateNote = (privateNote: PrivateNote): Promise<Response> => api.post('/private_notes', privateNote);
+const PRIVATE_NOTES = '/private_notes';
+
+const createPrivateNote = (privateNote: PrivateNote): Promise<Response> => api.post(PRIVATE_NOTES, privateNote);
 
 const updatePrivateNote = (privateNote: PrivateNote, id: string): Promise<Response> =>
-  api.put(`private_notes/${id}`, privateNote);
+  api.put(`${PRIVATE_NOTES}/${id}`, privateNote);
 
-const listPrivateNotes = (userId: string): Promise<Response> => api.get(`/private_notes/${userId}`);
+const listPrivateNotes = (userId: string): Promise<Response> => api.get(`${PRIVATE_NOTES}/${userId}`);
 
 const listPrivateNotesByMeeting = (userId: string, meetingId: string): Promise<Response> =>
-  api.get(`/private_notes/${userId}/meeting/${meetingId}`);
+  api.get(`${PRIVATE_NOTES}/${userId}/meeting/${meetingId}`);
 
-const removePrivateNote = (id: string): Promise<Response> => api.delete(`/private_notes/${id}`);
+const removePrivateNote = (id: string): Promise<Response> => api.delete(`${PRIVATE_NOTES}/${id}`);
 
 export default {
   createPrivateNote,

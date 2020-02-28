7e38558d713578c7d9824c7823e8c85bb47358bf
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-28 13:19:10 -0300

    Creates route to list meetings by userId

--- server/controllers/MeetingController.ts
+++ server/controllers/MeetingController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express';
 import { Meeting } from '../models/Meeting';
 
-class UserController {
+class MeetingController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const meetings = await Meeting.find({ $or: [{ userId1: id }, { userId2: id }] });
+    return res.json(meetings);
+  }
+
   public async store(req: Request, res: Response): Promise<Response> {
     const meeting = await Meeting.create(req.body);
 
@@ -9,4 +16,4 @@ class UserController {
   }
 }
 
-export default new UserController();
+export default new MeetingController();
--- server/routes.ts
+++ server/routes.ts
@@ -7,6 +7,7 @@ const routes = Router();
 //routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
+routes.get('/meetings/:id', MeetingController.index);
 routes.post('/meetings', MeetingController.store);
 
 export default routes;

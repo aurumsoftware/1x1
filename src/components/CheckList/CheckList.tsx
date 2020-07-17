dd1c4ee025dacedf9962663eb3ac984c3cfac8f1
Márcio Santos <marcio.tbms@gmail.com>
2020-07-17 12:13:19 -0300

    Separate checklist item into another component

--- src/components/CheckList/CheckList.tsx
+++ src/components/CheckList/CheckList.tsx
@@ -1,12 +1,6 @@
-import React, { ReactElement, ChangeEvent } from 'react';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import UncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
-import CheckedIcon from '@material-ui/icons/CheckCircle';
-import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
-import { CheckListItem, FieldContainer } from './styles';
+import React, { ReactElement } from 'react';
 import { Task } from '../../../types';
-import TextInput from '../TextInput';
+import CheckListItem from './CheckListItem';
 
 interface Props {
   values?: Task[];
@@ -16,43 +10,7 @@ interface Props {
 
 const CheckList: React.FC<Props> = ({ values = [], onChange, onRemove }) => {
   const mapItem = (item: Task, index: number): ReactElement => {
-    const handleCheck = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean): void => {
-      onChange(index, { ...item, checked });
-    };
-
-    const handleChangeDescription = (e: ChangeEvent<HTMLInputElement>): void => {
-      onChange(index, { ...item, description: e.target.value });
-    };
-
-    const handleRemove = (): void => {
-      onRemove(index);
-    };
-
-    return (
-      <CheckListItem>
-        <FieldContainer>
-          <Checkbox
-            icon={<UncheckedIcon />}
-            checkedIcon={<CheckedIcon />}
-            name="check-item"
-            color="primary"
-            onChange={handleCheck}
-          />
-          <TextInput
-            id="meetingTitle"
-            name="meetingTitle"
-            fontSize="sm"
-            placeholder="Digite o título"
-            value={item.description}
-            onChange={handleChangeDescription}
-          />
-        </FieldContainer>
-
-        <IconButton color="primary" aria-label="delete" onClick={handleRemove}>
-          <DeleteOutlineIcon />
-        </IconButton>
-      </CheckListItem>
-    );
+    return <CheckListItem task={item} index={index} onChange={onChange} onRemove={onRemove} />;
   };
 
   return <>{values.map(mapItem)}</>;
--- src/components/CheckList/CheckListItem/styles.ts
+++ src/components/CheckList/CheckListItem/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const CheckListItem = styled.div`
+export const ItemContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;

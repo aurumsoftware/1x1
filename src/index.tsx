d4cde313e1fbc115675394b01e90ea2a2424ca37
Arthur Mota <arthurmotaa9@gmail.com>
2020-07-17 11:59:36 -0300

    updating react-datepicker style

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
+import './react-datepicker.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import { registerLocale, setDefaultLocale } from 'react-datepicker';
 import ptBR from 'date-fns/locale/pt-BR';

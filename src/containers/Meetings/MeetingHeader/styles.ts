e46f1b62459b21d4b1f10613b5d23d56c5aca8be
Arthur Mota <arthurmotaa9@gmail.com>
2020-07-17 10:45:35 -0300

    updating MeetingHeader padding

--- src/containers/Meetings/MeetingHeader/styles.ts
+++ src/containers/Meetings/MeetingHeader/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: ${({ theme }): string => theme.spacings.lg};
+  padding: ${({ theme }): string => `${theme.spacings.lg} 0`};
 `;
 
 export const Title = styled.div`

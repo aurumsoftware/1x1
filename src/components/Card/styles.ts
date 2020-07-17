aa83d993a7fbd94f811c5894c0cf10d7a0248818
Arthur Mota <arthurmotaa9@gmail.com>
2020-07-17 10:45:54 -0300

    updating border width to 2px

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -7,7 +7,7 @@ interface Props {
 
 export const StyledCard = styled(Card)<Props>`
   && {
-    border: 1px solid ${({ theme }): string => theme.palette.secondary.light};
+    border: 2px solid ${({ theme }): string => theme.palette.secondary.light};
     ${({ theme }): string => `padding: ${theme.spacings.md} ${theme.spacings.lg};`}
     box-shadow: ${({ clickable }): string => (clickable ? 'none' : '0 1px 8px 1px rgba(0,0,0,0.15)')};
     border-radius: 8px;
